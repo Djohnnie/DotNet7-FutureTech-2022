b6c787cddfb426e4b2d65aafc5cdf8ac2e78df81
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2022-06-21 11:34:02 +0200

    Added Upgrade Assistant examples for Console, WinForms & ASP.NET MVC

---
+++ FutureTech2022/FutureTech2022.UpgradeFrameworkAspNetMvc.backup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FutureTech2022.UpgradeFrameworkAspNetMvc.MvcApplication" Language="C#" %>
---
+++ FutureTech2022/FutureTech2022.UpgradeFrameworkConsole.backup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FutureTech2022.UpgradeFrameworkConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.MachineName);
+        }
+    }
+}
